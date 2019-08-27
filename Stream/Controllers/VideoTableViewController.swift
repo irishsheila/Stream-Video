a7e715e345d97b1868d8bb8024653298db3e1a9f
Sheila Doherty <sheila.doherty@me.com>
2019-08-27 13:41:34 -0600

    Cleanup

--- Stream/Controllers/VideoTableViewController.swift
+++ Stream/Controllers/VideoTableViewController.swift
@@ -17,7 +17,6 @@ class VideoTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupNavBar()
         setupTableView()
         fetchData()
